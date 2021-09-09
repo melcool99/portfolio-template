f0607b2f8878aadd03b4dea5f423cc6e577360c0
Melcool99 <vladcristian84@gmail.com>
2021-09-09 17:16:11 +0300

    fixed redirect issue

--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography,Paper } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
